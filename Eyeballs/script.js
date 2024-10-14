d8a5ecc621ae277eaa57c1bdfa1d0bfe5084dfde
Warisha Jahanzaib <149045646+warisha-jahan@users.noreply.github.com>
2024-10-14 19:18:14 +0500

    Add files via upload

---
+++ Eyeballs/script.js
@@ -0,0 +1,44 @@
+gsap.to(".eyeball",{ 
+    duration: 1, 
+    left: "20px",
+    repeat: -1,  // -1 makes it repeat infinitely
+    yoyo: true,   // Makes the animation reverse back and forth
+    repeatDelay: 1 
+});
+
+gsap.to(".e1",{
+    duration: 1, 
+   rotate:25,
+   repeat:-1,
+   yoyo:true,
+   repeatDelay:1
+})
+
+gsap.to(".e2",{
+    duration: 1, 
+   rotate:-35,
+   repeat:-1,
+   yoyo:true,
+   repeatDelay:1
+})
+gsap.to(".e3",{
+    duration: 1, 
+   rotate:-10,
+   repeat:-1,
+   yoyo:true,
+   repeatDelay:1
+})
+gsap.to(".e4",{
+    duration: 1, 
+   rotate:30,
+   repeat:-1,
+   yoyo:true,
+   repeatDelay:1
+})
+gsap.to(".e5",{
+    duration: 1, 
+   rotate:-30,
+   repeat:-1,
+   yoyo:true,
+   repeatDelay:1
+})
